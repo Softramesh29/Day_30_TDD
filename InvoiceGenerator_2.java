8215c07acf1433a501fbd908311636a4d4a54d2f
softramesh29 <softramesh29@gmail.com>
2022-05-22 18:39:55 +0530

    Added: Claculating Fare for multiple ride file added along with test case file

---
+++ InvoiceGenerator_2.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.day_30_tdd;
+public class InvoiceGenerator_2 {
+
+	private static final int COST_PER_TIME = 1;
+	private static final double MINIMUM_COST_PER_KILOMETER = 10;
+	private static final double MINIMUM_FARE = 5;
+
+	public double CalculateFare(double distance, int time) {
+		double totalFare =  distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+		if(totalFare<MINIMUM_FARE)
+			return MINIMUM_FARE;
+		return totalFare;
+	}
+	public double CalculateFare(Ride[] rides) {
+		double totalFare = 0;
+		for (Ride ride : rides) {
+			totalFare += this.CalculateFare(ride.distance, ride.time);
+		}
+		return totalFare;
+	}
+	
+}
---
+++ Ride.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.day_30_tdd;
+
+public class Ride {
+	public double distance;
+	public int time;
+
+	public Ride(double distance, int time) {
+		this.distance = distance;
+		this.time = time;
+	}
+}
